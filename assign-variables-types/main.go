6a7cf865608fc901eb637cd14209fb7cb1011e99
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-27 13:06:05 -0700

    Declaring variables and scope

---
+++ assign-variables-types/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var y = 52
+var z string = "Which craft and Wizardry"
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y) // Printing out the values type
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+	// When you declare a varibles type you can not change the value to something that does not equel it's type
+}
