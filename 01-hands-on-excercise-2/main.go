19ac9c86e8b1e32e873b6efaf19491eaa24b0cd4
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-27 23:18:32 -0700

    Section one excersise two complete

--- 01-hands-on-excercise-2/main.go
+++ 01-hands-on-excercise-2/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+// 1. Assign types
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x, y, z) // 2a. Print out values of identifiers
+	// 2b. The compiler assigned values to the variables. What are these values called?
+	// Answer: "Zero Values" - sense they are not assigned to anything
 }
