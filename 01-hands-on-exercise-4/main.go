b0bb0872e4bfd8a3b8c23e14f9c032b64bd61a40
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-27 23:36:26 -0700

    Section one exercise four complete

--- 01-hands-on-exercise-4/main.go
+++ 01-hands-on-exercise-4/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+// 1. Create custom type
+type customType int
+
+// 2. create a VARIABLE of your new TYPE with the IDENTIFIER “x” using the “VAR” keyword
+var x customType
+
 func main() {
+	// 3a. print out the value of the variable “x”
+	fmt.Println(x)
+	// 3b. print out the type of the variable “x”
+	fmt.Printf("%T\n", x)
+	// 3c. assign 42 to the VARIABLE “x” using the “=” OPERATOR
+	x = 42
+	// 3d. print out the value of the variable “x”
+	fmt.Println(x)
 
 }
