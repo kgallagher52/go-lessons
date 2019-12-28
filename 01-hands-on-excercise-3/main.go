d98ddf09263c1881dd7178775176e5d504d00c61
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-27 23:29:39 -0700

    Section one exercise three complete

--- 01-hands-on-excercise-3/main.go
+++ 01-hands-on-excercise-3/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+var x int
+var y string
+var z bool
+
+func main() {
+	// 1. Assign values to variables
+	x = 42
+	y = "James Bond"
+	z = true
+	fmt.Println(x, y, z)
+	// 2. Second part
+	s := fmt.Sprintf("%v\t%v\t%v\t", x, y, z)
+	fmt.Println(s)
 }
