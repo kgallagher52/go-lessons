9d8179dd7d778994f4e87313b9212b2924814d7e
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-27 22:26:47 -0700

    Creating custom types

---
+++ creating-types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var a int
+
+type wizardAge int // Creating custom type
+
+var b wizardAge // Declaring b to custom type
+
+func main() {
+	a = 52
+	b = 200 // Assigning value to b which is a custom type = int
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+}
