2b654988efa701a351035dda6eb195630c0a82d2
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-27 23:09:57 -0700

    Added all of the excercises for the first section and did the first excercise

---
+++ 01-conversions-not-casting/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var a int
+
+type wizardAge int // Creating custom type
+
+var b wizardAge // Declaring b to custom type
+
+func main() {
+	a = 52
+	b = 200 // Assigning value to b which is a custom type = int
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+	// a = b This won't work
+	a = int(b) // Now b is a type integer by converting
+	fmt.Printf("%T\n", a)
+
+}
