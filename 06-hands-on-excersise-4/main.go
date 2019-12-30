5e5ecf54f49373b5bc0195cab3b6318f641ad89e
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-29 23:05:11 -0700

    Solution four

--- 06-hands-on-excersise-4/main.go
+++ 06-hands-on-excersise-4/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	// 1. assigns an int to a variable
+	a := 52
+	// 2. prints that int in decimal, binary, and hex
+	fmt.Println(a)
+	fmt.Printf("%d\n%b\n%x\n", a, a, a)
+	// 3. shifts the bits of that int over 1 position to the left, and assigns that to a
+	b := a << 1
+	fmt.Printf("%d\n%b\n%x\n", b, b, b)
 
 }
