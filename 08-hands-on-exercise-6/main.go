7e16f815b211c945016d471511d111de4727ee7b
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:36:38 -0700

    Section Eight six Solved

--- 08-hands-on-exercise-6/main.go
+++ 08-hands-on-exercise-6/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+// Write a program that shows the if statement in action
+func main() {
+	x := "Harry Potter"
+	if x == "Harry Potter" {
+		fmt.Println(x)
+	}
 }
