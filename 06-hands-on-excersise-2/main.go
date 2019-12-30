be4b7748c762f254715b9ced6841451e28695a5f
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-29 22:55:57 -0700

    Solution two

--- 06-hands-on-excersise-2/main.go
+++ 06-hands-on-excersise-2/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	a := 33 == 30
+	b := 21 <= 29
+	c := 22 >= 44
+	d := 22 != 13
+	e := 22 < 43
+	f := 25 > 47
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
 
 }
