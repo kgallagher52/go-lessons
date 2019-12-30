3d84a6317729b0c21438daf9d639c45e3820725a
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:34:34 -0700

    Section Eight Five Solved

--- 08-hands-on-exercise-5/main.go
+++ 08-hands-on-exercise-5/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+// Print out the remainder (Modulus) which is found for each number between 10 and 100 when divided by 4
+func main() {
+	for i := 10; i <= 100; i++ {
+		fmt.Printf("When %v is divided by 4, the remainder (aka modulus) is %v\n", i, i%4)
+	}
 }
