b83c5a4e6d0e6fdc70cecef133243e29eafda050
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:15:04 -0700

    Section Eight Three Solved

--- 08-hands-on-exercise-3/main.go
+++ 08-hands-on-exercise-3/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+// Create a for loop with this syntax "for condition {}" Have it print out the years you have been alive
+func main() {
+	birth := 1993
+	today := 2020
+	for birth <= today {
+		fmt.Printf("%d\n", birth)
+		birth++
+	}
 }
