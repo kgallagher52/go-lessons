242cf056a2a9d8df0f5b3f47851be0fa8ca70a92
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:05:47 -0700

    Section Eight Two Solved

--- 08-hands-on-exercise-2/main.go
+++ 08-hands-on-exercise-2/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+// Printing every rune code point of the uppercase alphabet three times
+func main() {
+	for i := 65; i < 91; i++ {
+		fmt.Printf("%d\n", i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("\t%#U\n", i)
+		}
+	}
 }
