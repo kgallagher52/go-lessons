9d6e946ea284182f8aea7ceaba645c1f7885bde0
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:49:50 -0700

    Section Eight Nine Solved

--- 08-hands-on-exercise-9/main.go
+++ 08-hands-on-exercise-9/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+// Create a program that uses a switch statement with the switch expression specified as a variable of TYPE string with the IDENTIFIER “favSport"
+
+func main() {
+	favSport := "Snowboarding"
+
+	switch favSport {
+	case "Skating":
+		fmt.Println("Negative")
+	case "Golf":
+		fmt.Println("Negative")
+	case "Snowboarding":
+		fmt.Println("Postive")
+	case "Football":
+		fmt.Println("Negative")
+	}
 }
