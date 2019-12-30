130ae87b1c66ced19d5a3fca53d8b48272e72be0
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:44:46 -0700

    Section Eight Eight Solved

--- 08-hands-on-exercise-8/main.go
+++ 08-hands-on-exercise-8/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+// Create a program that uses a switch statement with no switch expression specified.
+func main() {
+	switch {
+	case false:
+		fmt.Println("Won't Print")
+	case false:
+		fmt.Println("Won't Print")
+	case true:
+		fmt.Println("Will Print!")
+	}
 }
