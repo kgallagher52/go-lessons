5c53f00e24c143bfcd9e1f28391499322c5c8037
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:41:37 -0700

    Section Eight Seven Solved

--- 08-hands-on-exercise-7/main.go
+++ 08-hands-on-exercise-7/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+// Build of previous exercise, create a program that uses "else if" and "else"
+func main() {
+	x := "Harry Potter"
+	if x != "Harry Potter" {
+		fmt.Println(x)
+	} else if x == "Harry Potter" {
+		fmt.Println("if else", x)
+	} else {
+		fmt.Println("X does not equal the conditions")
+	}
 }
