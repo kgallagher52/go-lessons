0f32d792c546268d2ece7f5893135e3a14f6a216
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 16:27:30 -0700

    Slices & Composite Literal

---
+++ 10-slice-composite-literal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := type{values} // Composite literal
+	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(x)
+}
+
+// * Slice allows you to group together VALUES of the same TYPE
