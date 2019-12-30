1caa82b98aeda2cc766feda08c2e8bd9e0329746
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-30 15:51:57 -0700

    Section Eight Ten Solved

--- 08-hands-on-exercise-10/main.go
+++ 08-hands-on-exercise-10/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+// Write down what these print:
+// fmt.Println(true && true)
+// fmt.Println(true && false)
+// fmt.Println(true || true)
+// fmt.Println(true || false)
+// fmt.Println(!true)
+func main() {
+	fmt.Println(true && true)  // true
+	fmt.Println(true && false) // false
+	fmt.Println(true || true)  // true
+	fmt.Println(true || false) // true
+	fmt.Println(!true)         // false
 }
