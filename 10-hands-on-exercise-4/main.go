ad8f6121682a6d8607101ed2c21bddf96ce75561
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-30 21:23:39 -0700

    Section ten exercise four complete

--- 10-hands-on-exercise-4/main.go
+++ 10-hands-on-exercise-4/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+	// 1
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	// 2
+	x = append(x, 52)
+	// 3
+	fmt.Println(x)
+	// 4
+	x = append(x, 53, 54, 55)
+	// 5
+	fmt.Println(x)
+	// 6
+	y := []int{56, 57, 58, 59, 60}
+	x = append(x, y...)
+	// 7
+	fmt.Println(x)
 
 }
