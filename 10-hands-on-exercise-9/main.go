9ca0ed5b4693a0ca8af46271c1d30a326d281776
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-30 22:17:43 -0700

    Section ten exercise nine complete

--- 10-hands-on-exercise-9/main.go
+++ 10-hands-on-exercise-9/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	// 1
+	m := map[string][]string{ // * ** Complex Mapping
+		"Harry_Potter":    []string{"Hagrid", "Alavanders", "Ron"},
+		"Keaton_Gallager": []string{"Snowboarding", "GF", "Movies"},
+		"James Franco":    []string{"Acting", "Lauging", "Money"},
+	}
+	fmt.Println(m)
+
+	m["Jeremy Ranck"] = []string{"Gym", "Overwatch", "Utah Utes"}
+	fmt.Println("Record added", m)
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
 
 }
