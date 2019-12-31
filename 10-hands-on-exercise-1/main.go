ed8eb81148806e3c53edc0ebc25c01b70cdae1f3
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-30 20:57:31 -0700

    Section ten exercise one complete

--- 10-hands-on-exercise-1/main.go
+++ 10-hands-on-exercise-1/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 1.
+	x := [5]int{10, 11, 12, 13, 14}
+
+	// 2.
+	for _, v := range x {
+		fmt.Println(v)
+	}
+
+	// 3a.
+	fmt.Printf("%T\n", x)
 }
