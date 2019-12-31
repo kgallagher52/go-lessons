bbdf4af168d1af97743dfb3ae7aa959101390c90
Keaton Gallagher <d00249750@dmail.dixie.edu>
2019-12-30 21:57:10 -0700

    Section ten exercise seven complete

--- 10-hands-on-exercise-7/main.go
+++ 10-hands-on-exercise-7/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 1
+	fs := []string{"James", "Bond", "Shaken, not stirred"}
+	ss := []string{"Miss", "Moneypenny", "Hellooooo, James"}
+	fmt.Println(fs)
+	fmt.Println(ss)
+
+	ms := [][]string{fs, ss}
+	fmt.Println(ms)
+
+	for _, v := range ms {
+		fmt.Println(v)
+		for _, sv := range v {
+			fmt.Println(sv)
+		}
+	}
 }
