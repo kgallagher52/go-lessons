093632fca9e0098db76f2dfd037f23fc9c707ed1
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-02 22:56:29 -0700

    Completed exercise four of section twelve, creating a anonymous struct

--- 12-hands-on-exercise-4/main.go
+++ 12-hands-on-exercise-4/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+// 1. Create and use an anonymous struct.
+
+func main() {
+	wizard := struct {
+		name  string
+		color string
+		age   int
+	}{
+		name:  "Gandoff",
+		color: "Grey",
+		age:   200,
+	}
+	fmt.Println("Anonymous Struct: ", wizard)
+	fmt.Printf("%T\n", wizard)
 }
