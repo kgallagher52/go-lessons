79f83c44081ab4e56ccacd9b643277057a7ccd22
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-02 21:23:30 -0700

    General things that are good to know with Golang

--- 12-houskeeping/main.go
+++ 12-houskeeping/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+// we create VALUES of a certain TYPE that are stored in VARIABLES
+// and those VARIABLES have IDENTIFIERS
+
+var x int
+
+type person struct {
+	first string
+	last  string
+}
+
+type foo int
+
+var y foo
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+	}
+	fmt.Println("Hello, playground", p1)
 }
