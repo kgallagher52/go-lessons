9cf99ce1be47d29e8909d671bf950a93e245fdd5
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-02 22:15:06 -0700

    Exercise two completed

--- 12-hands-on-exercise-2/main.go
+++ 12-hands-on-exercise-2/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
+type person struct {
+	first      string
+	last       string
+	favFlavors []string
+}
+
 func main() {
+	p1 := person{
+		first: "Keaton",
+		last:  "Gallagher",
+		favFlavors: []string{
+			"Mint",
+			"vanilla",
+		},
+	}
+	// 1. Take the code from the previous exercise, then store the values of type person in a map with the key of last name. Access each value in the map. Print out the values, ranging over the slice.
+
+	m := map[string]person{ // p1.last = key | p1 = value you want for the record found
+		p1.last: p1,
+	}
+	fmt.Println("Map of last name value\n", m)
 
 }
