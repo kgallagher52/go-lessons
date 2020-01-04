402d6ed9a8aaf1797a4ee452e343848ad6ddd8a2
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-04 09:56:56 -0700

    Completed exercise seven of section 14 assigning a function to a identifer

--- 14-functions/hands-on-exercise-7/main.go
+++ 14-functions/hands-on-exercise-7/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+)
+
+var x int
+var g func() // Defined the type as a function
+
 func main() {
 
+	f := func() { // Made f identifer which is type function
+		for i := 0; i < 3; i++ {
+			fmt.Println(i)
+		}
+	}
+
+	f() // Calling f which is underlyn type func
+	fmt.Printf("%T\n", f)
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	g = f
+	g()
+	fmt.Printf("this is g %T\n", g)
+
+	fmt.Println("done")
 }
