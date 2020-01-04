62d2606ac7c43ed0c6fe2bc5110fa00ccc51e96c
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-04 05:45:29 -0700

    Showing how defer makes the funciton it's being used on run on close

--- 14-functions/hands-on-exercise-3/main.go
+++ 14-functions/hands-on-exercise-3/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	first()
+	second()
+	// 1. Use the “defer” keyword to show that a deferred func runs after the func containing it exits.
+	defer first()
+	second()
+}
+
+func first() {
+	fmt.Println("This function should run first!!!")
+}
+func second() {
+	fmt.Println("This function should run second!!!")
 }
--- 14-functions/main.go
+++ 14-functions/main.go
@@ -2,15 +2,16 @@ package main
 
 import "fmt"
 
-var s = 484  // Seperation
+var s = 483  // Seperation
 var tb = 772 // Score to Beat
 
 func main() {
-	gk := 19 // Game Kills
-	gd := 10 // Game Deaths
+	gk := 13 // Game Kills
+	gd := 17 // Game Deaths
 
 	if gk > gd {
 		fmt.Println("Good Game!", (gk-gd)+s)
+		// fmt.Println(8197 - 7710)
 	} else {
 		fmt.Println("Bad Game!", s-(gd-gk))
 	}
