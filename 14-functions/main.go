1ad67f20b86bebf2a228617e04587a242fcf6a2b
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-04 11:14:04 -0700

    Completed exercise nine in section 14 callback functions

--- 14-functions/main.go
+++ 14-functions/main.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-var s = 634  // Seperation
+var s = 641  // Seperation
 var tb = 772 // Score to Beat
 
 func main() {
-	gk := 15 // Game Kills
-	gd := 19 // Game Deaths
+	gk := 33 // Game Kills
+	gd := 20 // Game Deaths
 
 	if gk > gd {
 		fmt.Println("Good Game!", (gk-gd)+s)
-		// fmt.Println("Manual Sores:", 8633-8023)
+		// fmt.Println("Manual Sores:", 8783-8142)
 	} else if gk == gd {
 		fmt.Println("It's not bad but not good", s)
 	} else {
