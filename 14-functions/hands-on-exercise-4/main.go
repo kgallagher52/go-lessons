7f50c3320d4c2bb05756ddce03d42b7205021eb8
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-04 07:33:29 -0700

    Completed exercise four, making method and struct

--- 14-functions/hands-on-exercise-4/main.go
+++ 14-functions/hands-on-exercise-4/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+// 1. Create a user defined struct with  the identifier “person”
+// the fields:
+// first
+// last
+// age
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+//2 	attach a method to type person with
+// the identifier “speak”
+// the method should have the person say their name and age
+func (p person) speak() {
+	fmt.Println("Hi I am ", p.first, " ", p.last, "I am ", p.age, "years old!")
+}
+
 func main() {
+
+	p1 := person{ // 3. create a value of type person
+
+		first: "Keaton",
+		last:  "Gallagher",
+		age:   26,
+	}
+	p1.speak() // 4. call the method from the value of type person
 
 }
--- 14-functions/main.go
+++ 14-functions/main.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
-var s = 483  // Seperation
+var s = 545  // Seperation
 var tb = 772 // Score to Beat
 
 func main() {
-	gk := 13 // Game Kills
-	gd := 17 // Game Deaths
+	gk := 17 // Game Kills
+	gd := 18 // Game Deaths
 
 	if gk > gd {
 		fmt.Println("Good Game!", (gk-gd)+s)
-		// fmt.Println(8197 - 7710)
+		// fmt.Println(8247 - 7744)
+	} else if gk == gd {
+		fmt.Println("It's not bad but not good", s)
 	} else {
 		fmt.Println("Bad Game!", s-(gd-gk))
 	}
+
+	defer fmt.Println("You are ", tb-s, "away from catching up!")
 }
