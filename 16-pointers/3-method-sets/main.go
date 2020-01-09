b46fc572da5bb308f09a669df4e87544edbdcdc6
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-09 01:25:32 -0700

    Talking about how pointer can't go to a non pointer value

--- 16-pointers/3-method-sets/main.go
+++ 16-pointers/3-method-sets/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "math"
 
+import "fmt"
+
+type shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+// Functions with method area
+// Non poi
+func (c *circle) area() float64 { // Is of type shape
+	return math.Pi * c.radius * c.radius
+}
+
+// Function
+
+func info(s shape) {
+	fmt.Println("area", s.area())
+}
+
+func main() {
+	c := circle{9}
+	fmt.Printf("%T\n", &c)
+	info(&c)
 }
