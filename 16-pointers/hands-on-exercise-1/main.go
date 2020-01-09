d1b585eedabc88a728c6fc567a89812038ee0ff6
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-09 01:38:18 -0700

    exercise one finished

--- 16-pointers/hands-on-exercise-1/main.go
+++ 16-pointers/hands-on-exercise-1/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// 1. Create a value and assign it to a variable.
+	x := 45
+	// 2. Print the address of that value.
+	fmt.Println("Address of value x: \n", &x)
 }
