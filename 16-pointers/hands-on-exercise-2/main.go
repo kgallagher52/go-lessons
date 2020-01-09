1df067fc873c5c2ac97e0cf38fb7c17ff891d5bd
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-09 01:47:45 -0700

    exercise two complete of section 16

--- 16-pointers/hands-on-exercise-2/main.go
+++ 16-pointers/hands-on-exercise-2/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct { // 1. create a person struct
+	name string
+}
+
+func main() {
+	p1 := person{
+		name: "Harry Potter",
+	}
+	fmt.Println(p1)
+	changeMe(&p1)
+	fmt.Println(p1)
+}
+
+func changeMe(p *person) { // 2. create a func called “changeMe” with a *person as a parameter
+	p.name = "Mr.Tom" // 2a. change the value stored at the *person address
+	// (*p).name = "Tommy"
 }
