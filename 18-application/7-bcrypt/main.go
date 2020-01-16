3ab565aa8027b0052a14ece2dd0a011e82c894e4
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-16 00:45:33 -0700

    Working with bcrypt in Golang

--- 18-application/7-bcrypt/main.go
+++ 18-application/7-bcrypt/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
 func main() {
-	
+	p := `mySecretPassword`
+	fmt.Println("Not hashed Pass:", p)
+
+	hp, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Hashed Pass:", hp)
+
+	cp := "mySecretPassword"
+
+	// Example of it working
+	if err = bcrypt.CompareHashAndPassword(hp, []byte(cp)); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("cp - correct password")
+	}
+
+	wp := "wrong password"
+
+	// Example of it not working
+	if err = bcrypt.CompareHashAndPassword(hp, []byte(wp)); err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
