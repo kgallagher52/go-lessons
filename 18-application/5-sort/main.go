4107d90807adb4bf682cfe975626c815d0a271da
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-15 23:38:17 -0700

    using the sort package, and understanding that if the function takes in something but does not return anything that you don't need to assign any variable to that sense it's not returning anything

--- 18-application/5-sort/main.go
+++ 18-application/5-sort/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
-	
+	xi := []int{10, 0, 1, 100, 52, 14, 37}
+	xs := []string{"Katara", "Zuko", "Toph", "Sakka", "Momo", "Aang"}
+
+	fmt.Println(xi)
+	fmt.Println(xs)
+
+	// ** the sort function takes in (a []int) but does not return so we do not need to assign paramater to this
+	sort.Ints(xi)
+	fmt.Println(xi)
+
+	sort.Strings(xs)
+	fmt.Println(xs)
+
 }
