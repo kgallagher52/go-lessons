8634e2ea22767212a2aef1ca62d41d3692955e8e
Keaton Gallagher <d00249750@dmail.dixie.edu>
2020-01-16 01:13:01 -0700

    fixed instructions for exercise one

--- 18-application/hands-on-exercise-1/main.go
+++ 18-application/hands-on-exercise-1/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type wizard struct {
+	First string
+	Age   int
+}
+
+func main() {
+	w1 := wizard{
+		First: "Harry",
+		Age:   21,
+	}
+
+	w2 := wizard{
+		First: "Hermine",
+		Age:   22,
+	}
+
+	w3 := wizard{
+		First: "Ron",
+		Age:   20,
+	}
+
+	wizards := []wizard{w1, w2, w3}
+
+	fmt.Println(wizards)
+
+	// your code goes here
 }
